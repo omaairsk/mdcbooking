7dc746fcb9ca8990566804a410c8223cac135697
iamfardinsk <139180541+omaairsk@users.noreply.github.com>
2026-01-17 00:44:40 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -5,8 +5,13 @@ const router = express.Router();
 router.post("/login", (req, res) => {
   const { email, password } = req.body;
 
-  if (email === "admin@mdc.com" && password === "StrongPass@123") {
-    const token = jwt.sign({ role: "admin" }, process.env.JWT_SECRET, { expiresIn: "8h" });
+  if (
+    email === process.env.ADMIN_EMAIL &&
+    password === process.env.ADMIN_PASSWORD
+  ) {
+    const token = jwt.sign({ role: "admin" }, process.env.JWT_SECRET, {
+      expiresIn: "8h"
+    });
     return res.json({ token });
   }
 
