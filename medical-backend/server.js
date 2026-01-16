1f05de1163d32caa8dad498fe000606e223e5174
iamfardinsk <139180541+omaairsk@users.noreply.github.com>
2026-01-17 00:46:24 +0530

    Update server.js

--- medical-backend/server.js
+++ medical-backend/server.js
@@ -2,18 +2,18 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
-const authRoutes = require("./routes/auth");
-const bookingRoutes = require("./routes/booking");
-const adminRoutes = require("./routes/admin");
+const booking = require("./routes/booking");
+const auth = require("./routes/auth");
+const admin = require("./routes/admin");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/auth", authRoutes);
-app.use("/api/booking", bookingRoutes);
-app.use("/api/admin", adminRoutes);
+app.use("/api/booking", booking);
+app.use("/api/auth", auth);
+app.use("/api/admin", admin);
 
 app.listen(process.env.PORT, () => {
   console.log("Backend running on port", process.env.PORT);
